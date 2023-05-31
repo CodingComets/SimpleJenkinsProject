472db7b0a5fbabe8be5f5edb36ff83284539d5d5
Diego <Diegoalizo75@gmail.com>
2023-05-30 21:18:29 -0400

    Grammar Correction

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         for(int i=0; i<10; i++){
-            System.out.println("Hello, World!");
+            System.out.println("Hello, Ladies!");
             Thread.sleep(2000);
         }
     }
